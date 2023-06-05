2e588c6ae02aeba4709c7242d033594f5896a7ec
Václav Kříž <krizacek98@gmail.com>
2023-06-05 15:05:08 +0200

    x

--- v2_spec/pactjs_v10_nestjspact_v3/provider/src/app.module.ts
+++ v2_spec/pactjs_v10_nestjspact_v3/provider/src/app.module.ts
@@ -1,11 +1,11 @@
-import { Module } from "@nestjs/common"
-import { AppController } from "./app.controller"
-import { AppService } from "./app.service"
-import { AppRepository } from "./app.repository"
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppRepository } from "./app.repository";
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService, AppRepository],
-  exports: [AppRepository],
+    controllers: [ AppController ],
+    providers: [ AppRepository ],
+    exports: [ AppRepository ],
 })
-export class AppModule {}
+export class AppModule {
+}
