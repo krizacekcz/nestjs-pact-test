ee08d7827447ff10e40e6851a105d6bd59e007b8
Václav Kříž <krizacek98@gmail.com>
2023-06-05 15:01:12 +0200

    contract testing

--- v2_spec/pactjs_v10_nestjspact_v3/consumer/src/app.controller.ts
+++ v2_spec/pactjs_v10_nestjspact_v3/consumer/src/app.controller.ts
@@ -1,20 +1,20 @@
-import { BadRequestException, Controller, Get, Param } from "@nestjs/common"
-import { AppService } from "./app.service"
-import { Animal } from "./animal.interface"
+import { BadRequestException, Controller, Get, Param } from "@nestjs/common";
+import { AppService } from "./app.service";
+import { Animal } from "./animal.interface";
 
 @Controller("/")
 export class AppController {
-  public constructor(private readonly appService: AppService) {}
-
-  @Get("/suggestions/:animalId")
-  public async getAllAnimals(
-    @Param("animalId") animalId: string
-  ): Promise<Animal> {
-    try {
-      const animal = await this.appService.getAnimalById(animalId)
-      return await this.appService.suggestion(animal)
-    } catch (e) {
-      throw new BadRequestException()
+    public constructor(private readonly appService: AppService) {
     }
-  }
+
+    @Get("/:animalId")
+    public async getAllAnimals(
+        @Param("animalId") animalId: string
+    ): Promise<Animal> {
+        try {
+            return this.appService.getAnimalById(animalId);
+        } catch (e) {
+            throw new BadRequestException();
+        }
+    }
 }
